a5f8dd44d542a206bc5d3cf41e9872b78eee55d3
Yomal Bandara <yomalb07@gmail.com>
2022-04-16 19:17:55 +0530

    Added the read method

--- src/main/java/com/ItemService.java
+++ src/main/java/com/ItemService.java
@@ -1,5 +1,5 @@
 package com;
 
 public class ItemService {
-
+	
 }
--- src/main/java/model/Item.java
+++ src/main/java/model/Item.java
@@ -3,6 +3,8 @@ package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Item {
 		//A common method to connect to the DB
@@ -23,7 +25,7 @@ public class Item {
 			 return con; 
 		 } 
 		
-		//Insert method implemented
+		//Insert method 
 		public String insertItem(String code, String name, String price, String desc) 
 		 { 
 			 String output = ""; 
@@ -57,110 +59,59 @@ public class Item {
 			 return output; 
 		 } 
 		
-		
-	public String readItems() 
-	 { 
-	 String output = ""; 
-	 try
-	 { 
-	 Connection con = connect(); 
-	 if (con == null) 
-	 {return "Error while connecting to the database for reading."; } 
-	 // Prepare the html table to be displayed
-	 output = "<table border='1'><tr><th>Item Code</th><th>Item Name</th>" +
-	 "<th>Item Price</th>" + 
-	 "<th>Item Description</th>" +
-	 "<th>Update</th><th>Remove</th></tr>"; 
-	 
-	 String query = "select * from items"; 
-	 Statement stmt = con.createStatement(); 
-	 ResultSet rs = stmt.executeQuery(query); 
-	 // iterate through the rows in the result set
-	 while (rs.next()) 
-	 { 
-	 String itemID = Integer.toString(rs.getInt("itemID")); 
-	 String itemCode = rs.getString("itemCode"); 
-	 String itemName = rs.getString("itemName"); 
-	 String itemPrice = Double.toString(rs.getDouble("itemPrice")); 
-	 String itemDesc = rs.getString("itemDesc"); 
-	 // Add into the html table
-	 output += "<tr><td>" + itemCode + "</td>"; 
-	 output += "<td>" + itemName + "</td>"; 
-	 output += "<td>" + itemPrice + "</td>"; 
-	 output += "<td>" + itemDesc + "</td>"; 
-	 // buttons
-	 output += "<td><input name='btnUpdate' type='button' value='Update' 
-	 class='btn btn-secondary'></td>"
-	 + "<td><form method='post' action='items.jsp'>"
-	 + "<input name='btnRemove' type='submit' value='Remove' 
-	 class='btn btn-danger'>"
-	 + "<input name='itemID' type='hidden' value='" + itemID 
-	 + "'>" + "</form></td></tr>"; 
-	 } 
-	 con.close(); 
-	 // Complete the html table
-	 output += "</table>"; 
-	 } 
-	 catch (Exception e) 
-	 { 
-	 output = "Error while reading the items."; 
-	 System.err.println(e.getMessage()); 
-	 } 
-	 return output; 
-	 } 
-	public String updateItem(String ID, String code, String name, String price, String desc) 
-	{ 
-		 String output = ""; 
-		 try
+		//Read method
+		public String readItems() 
 		 { 
-		 Connection con = connect(); 
-		 if (con == null) 
-		 {return "Error while connecting to the database for updating."; } 
-		 // create a prepared statement
-		 String query = "UPDATE items SET itemCode=?,itemName=?,itemPrice=?,itemDesc=? 
-		 WHERE itemID=?"; 
-		 PreparedStatement preparedStmt = con.prepareStatement(query); 
-		 // binding values
-		 preparedStmt.setString(1, code); 
-		 preparedStmt.setString(2, name); 
-		 preparedStmt.setDouble(3, Double.parseDouble(price)); 
-		 preparedStmt.setString(4, desc); 
-		 preparedStmt.setInt(5, Integer.parseInt(ID)); 
-		 // execute the statement
-		 preparedStmt.execute(); 
-		 con.close(); 
-		 output = "Updated successfully"; 
-		 } 
-		 catch (Exception e) 
-		 { 
-		 output = "Error while updating the item."; 
-		 System.err.println(e.getMessage()); 
-		 } 
-		 return output; 
-		 } 
-		public String deleteItem(String itemID) 
-		 { 
-		 String output = ""; 
-		 try
-		 { 
-		 Connection con = connect(); 
-		 if (con == null) 
-		 {return "Error while connecting to the database for deleting."; } 
-		 // create a prepared statement
-		 String query = "delete from items where itemID=?"; 
-		 PreparedStatement preparedStmt = con.prepareStatement(query); 
-		 // binding values
-		 preparedStmt.setInt(1, Integer.parseInt(itemID)); 
-		 // execute the statement
-		 preparedStmt.execute(); 
-		 con.close(); 
-		 output = "Deleted successfully"; 
-		 } 
-		 catch (Exception e) 
-		 { 
-		 output = "Error while deleting the item."; 
-		 System.err.println(e.getMessage()); 
-		 } 
-		 return output; 
+			 String output = ""; 
+			 try
+			 { 
+				 Connection con = connect(); 
+				 if (con == null) 
+				 {
+					 return "Error while connecting to the database for reading."; 
+				 } 
+				 // Prepare the html table to be displayed
+				 output = "<table border='1'><tr><th>Item Code</th><th>Item Name</th>" +
+				 "<th>Item Price</th>" + 
+				 "<th>Item Description</th>" +
+				 "<th>Update</th><th>Remove</th></tr>"; 
+				 
+				 String query = "select * from items"; 
+				 Statement stmt = con.createStatement(); 
+				 ResultSet rs = stmt.executeQuery(query);
+				 
+				 
+				 // iterate through the rows in the result set
+				 while (rs.next()) 
+				 { 
+				 String itemID = Integer.toString(rs.getInt("itemID")); 
+				 String itemCode = rs.getString("itemCode"); 
+				 String itemName = rs.getString("itemName"); 
+				 String itemPrice = Double.toString(rs.getDouble("itemPrice")); 
+				 String itemDesc = rs.getString("itemDesc");
+				 
+				 // Add into the html table
+				 output += "<tr><td>" + itemCode + "</td>"; 
+				 output += "<td>" + itemName + "</td>"; 
+				 output += "<td>" + itemPrice + "</td>"; 
+				 output += "<td>" + itemDesc + "</td>";
+				 
+				 // buttons
+				 output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"
+				 + "<td><form method='post' action='items.jsp'>"
+				 + "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"
+				 + "<input name='itemID' type='hidden' value='" + itemID 
+				 + "'>" + "</form></td></tr>"; 
+				 } 
+				 con.close(); 
+				 // Complete the html table
+				 output += "</table>"; 
+			 } 
+			 catch (Exception e) 
+			 { 
+				 output = "Error while reading the items."; 
+				 System.err.println(e.getMessage()); 
+			 } 
+			 return output; 
 		 } 
 }
