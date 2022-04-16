5fa122c5bc6394034c2c354146f41eb5bc86cc40
Yomal Bandara <yomalb07@gmail.com>
2022-04-16 19:41:08 +0530

    Configure the servlets

--- src/main/java/com/ItemService.java
+++ src/main/java/com/ItemService.java
@@ -3,6 +3,30 @@ package com;
 //import the model class
 import model.Item;
 
+//For REST Service
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType;
+
+//For JSON
+import com.google.gson.*;
+
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document; 
+
+@Path("/Items")
 public class ItemService {
+	
+	//creating an object from the item class
+	Item itemObj = new Item();
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String raedItems()
+	{
+		return "Hello";
+	}
 	
 }
